2dcbb7076855dd9cfe712a88b7be09e146657357
zzc <zhichao.zhang@colorv.com>
2021-04-16 10:54:08 +0800

    feat:不同账号支持

---
+++ config/index.js
@@ -0,0 +1,12 @@
+module.exports = {
+  yd_key: '868480929',
+  userId: '',
+  base_url: '',
+  url: '',
+  account: '',
+  password: '',
+  mySshPass: '',
+  login_url: '',
+  aliyunPass: '',
+  aliaddress: ''
+}
